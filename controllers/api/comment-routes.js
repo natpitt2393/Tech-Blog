91f5762da8b3d75453e88b83d15d7623edd57fee
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-31 09:24:05 -0500

    completed code in comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const { Comment } = require('../../models/');
+const { beforeDestroy } = require('../../models/User');
 const withAuth = require('../../utils/auth');
 
 // URL: /api/comment
 router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
-      // TODO: COMMENT BODY IN REQUEST USING SPREAD
-
-      // TODO: SET USERID userId TO SESSION LOGGEDIN USERID
+      ...req.body,
+      userId: req.session.userId
     });
     res.json(newComment);
   } catch (err) {
